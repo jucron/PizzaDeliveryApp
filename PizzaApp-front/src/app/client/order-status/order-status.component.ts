cfc3234cb7ff556487c2dd9860af29cb641d0ce1
Joao Renault <Joaomarcelo.Renault@emea.merkleinc.com>
2023-01-04 09:32:23 +0000

    Fixed bug in order-status.component.ts -> updateOrder()

--- PizzaApp-front/src/app/client/order-status/order-status.component.ts
+++ PizzaApp-front/src/app/client/order-status/order-status.component.ts
@@ -1,6 +1,6 @@
 import {AfterViewInit, Component, OnInit} from '@angular/core';
 import {ClientService, Order} from "../client.service";
-import {interval} from "rxjs";
+import {interval, Subscription} from "rxjs";
 import {ClientOrderGuard} from "../routerguards/client-order.guard";
 
 @Component({
@@ -10,6 +10,7 @@ import {ClientOrderGuard} from "../routerguards/client-order.guard";
 })
 export class OrderStatusComponent implements OnInit, AfterViewInit {
   order: Order;
+  intervalSubscription: Subscription;
   statusCode = ['confirmed','accepted','baking','pizzaReady','delivering','pizzaDelivered','finished'];
   statusView = ['Confirmed','Accepted','Baking','Pizza Ready','Delivering','Delivered','Finished'];
 
@@ -28,12 +29,10 @@ export class OrderStatusComponent implements OnInit, AfterViewInit {
   async updateOrder() {
     let milliseconds = 20000;
     console.log('updateOrder: function started, will refresh page after '+milliseconds/1000+' seconds.')
-    let sub = interval(milliseconds).subscribe(() => {
+    this.intervalSubscription = interval(milliseconds).subscribe(() => {
       let currentUrl = this.clientService.getCurrentUrl();
       if (this.order.status=='finished' || currentUrl != '/client/order-status') {
-        // console.log(currentUrl)
-        console.log("updateOrder: function is no longer necessary, shutting it down.")
-        sub.unsubscribe();
+        this.closeIntervalSubscription();
       } else {
         console.log(milliseconds/1000+' seconds passed: refreshing page again with new Order')
         this.clientService.redirectTo('client/order-status',true);
@@ -43,6 +42,11 @@ export class OrderStatusComponent implements OnInit, AfterViewInit {
 
   assignOrder () {
     this.order = ClientOrderGuard.order;
+}
+
+closeIntervalSubscription () {
+    console.log("updateOrder: function is no longer necessary, shutting it down.")
+    this.intervalSubscription.unsubscribe();
 }
 
   closeTask() {
